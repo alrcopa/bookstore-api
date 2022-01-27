772b724ed460878d405f87a1aa9e6cd0d0685db5
alrcopa <alrcopa@hotmail.com>
2022-01-27 16:57:56 -0300

    first commit

---
+++ src/main/java/br/com/requeijo/bookstoreapi/BookstoreApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.requeijo.bookstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApiApplication.class, args);
+	}
+
+}
